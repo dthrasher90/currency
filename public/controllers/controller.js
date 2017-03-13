2dd8679a01e1f82bbbbfff7815ddc0554b1e218a
dthrasher90 <dthrasher90@GMAIL.COM>
2017-03-13 08:23:21 -0800

    update master
    update master

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,18 +2,113 @@
 var app = angular.module('currencyApp', []);
 
 
-app.controller('mainCtrl', function($scope, $http){
+app.controller('mainCtrl', function($scope, $http) {
 
-  $http.get('/currency').then(function( data){
-    console.log("i got the data", data);
-    $scope.currency = data;
-    console.log($scope.currency);
-  });
 
-  $scope.submit = function(){
-    alert("subit button pressed");
-    $http.post('/currency', $scope.ammount);
-    console.log($scope.ammount);
-  }
+    function refresh() {
+        location.reload();
+    }
+
+    $http.get('http://api.fixer.io/latest?base=USD').then(function(results) {
+        console.log();
+        $scope.GBPrate = results.data.rates.GBP;
+        $scope.EURrate = results.data.rates.EUR;
+        $scope.JPYrate = results.data.rates.JPY;
+        $scope.KRWrate = results.data.rates.KRW;
+        $scope.MXNrate = results.data.rates.MXN;
+        $scope.AUDrate = results.data.rates.AUD;
+        $scope.IDRrate = results.data.rates.IDR;
+        console.log($scope.EURrate, $scope.GBPrate, $scope.JPYrate, $scope.MXNrate);
+
+    });
+
+    $http.get('/currency').then(function(data) {
+        console.log("i got the data", data);
+        $scope.currency = data.data.data;
+        //onsole.log($scope.EURrate);
+    });
+
+    $scope.$watch('currencyIndex', function(newCurrencyIndex, oldCurrencyIndex) {
+
+        if ($scope.currencyIndex == 'AUD') {
+            $scope.currencyIndexRate = $scope.AUDrate;
+            console.log($scope.currencyIndexRate);
+        }
+        if ($scope.currencyIndex == 'EUR') {
+            $scope.currencyIndexRate = $scope.EURrate;
+            console.log($scope.currencyIndexRate);
+        }
+
+        if ($scope.currencyIndex == 'GBP') {
+            $scope.currencyIndexRate = $scope.GBPrate;
+            console.log($scope.currencyIndexRate);
+        }
+
+        if ($scope.currencyIndex == 'IDR') {
+            $scope.currencyIndexRate = $scope.IDRrate;
+            console.log($scope.currencyIndexRate);
+        }
+
+        if ($scope.currencyIndex == 'JPY') {
+            $scope.currencyIndexRate = $scope.JPYrate;
+            console.log($scope.currencyIndexRate);
+        }
+        if ($scope.currencyIndex == 'KRW') {
+            $scope.currencyIndexRate = $scope.KRWrate;
+            console.log($scope.currencyIndexRate);
+        }
+
+
+        if ($scope.currencyIndex == 'MXN') {
+            $scope.currencyIndexRate = $scope.MXNrate;
+            console.log($scope.currencyIndexRate);
+        }
+
+    })
+
+
+
+    $scope.submit = function() {
+        alert("subit button pressed");
+        console.log($scope.currencyIndexRate);
+        $scope.total = $scope.currencyIndexRate * $scope.amount;
+        $scope.total = $scope.total.toFixed(2);
+        console.log($scope.total)
+        $http.post('/currency', {
+                name: $scope.currencyIndex,
+                amount: $scope.amount,
+                rate: $scope.currencyIndexRate,
+                total: $scope.total
+            })
+            .then(function(response) {
+                console.log("post request made", response);
+
+            }, function(response) {
+                console.log("an error has occured");
+
+
+            });
+
+            refresh();
+
+    }
+
+    $scope.delete = function(data) {
+        // alert("delete button pressed");
+        console.log(data)
+        // pass id as data attribute
+        $http({
+            method: 'DELETE',
+            url: '/currency/' + data._id
+            // data:  req.params._id
+        }).then(function(response) {
+            console.log('delete request was made');
+        }, function(response) {
+            console.log("a delete error has occured");
+        });
+
+        refresh();
+    }
+
 
 });
--- server.js
+++ server.js
@@ -2,46 +2,86 @@ var express = require("express");
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var morgan = require('morgan');
-var mongoose= require('mongoose');
+var mongoose = require('mongoose');
 var Currency = require('./models/currency');
 
 var app = express();
 
-app.use(morgan('dev')); 
-app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({
+    'extended': 'true'
+})); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(bodyParser.json({
+    type: 'application/vnd.api+json'
+})); // parse application/vnd.api+json as json
+
 app.use(methodOverride());
 app.use(express.static(__dirname + '/public'));
 
 
 mongoose.connect('mongodb://localhost:27017/currency')
 
-mongoose.connection.on('connected', function () {
-  console.log('Mongoose default connection open to currency');
-}); 
-
-
-app.post('/currency', function(req, res){
-  var newCurrency = new Currency(req.body);
-  newCurrency.save(function(err, result) {
-    if(err){
-      res.json({status: false, error: "Currency not saved"});
-			return;
-    }
-    res.json({status: true, data: result});
-  })
+mongoose.connection.on('connected', function() {
+    console.log('Mongoose default connection open to currency');
 });
 
-app.get('/currency', function(req, res){
-  Currency.find(function(err, result){
-    if(err){
-      res.json({status: false, error: "Error in finding currency"});
-			return;
-    }
-    res.json({status: true, data: result});
-  })
+
+app.post('/currency', function(req, res) {
+
+    var newCurrency = new Currency({
+        name: req.body.name,
+        amount: req.body.amount,
+        rate: req.body.rate,
+        total: req.body.total
+    })
+    newCurrency.save(function(err, result) {
+        if (err) {
+            res.json({
+                status: false,
+                error: "Currency not saved"
+            });
+            return;
+        }
+        res.json({
+            status: true,
+
+        });
+    })
 });
+
+app.get('/currency', function(req, res) {
+    Currency.find(function(err, result) {
+        if (err) {
+            res.json({
+                status: false,
+                error: "Error in finding currency"
+            });
+            return;
+        }
+        res.json({
+            status: true,
+            data: result
+        });
+    })
+});
+
+app.delete('/currency/:id', function(req, res) {
+  console.log(req.params)
+
+    Currency.findByIdAndRemove({
+        _id: req.params.id
+    }, function(err, result) {
+        if (err)
+            res.send(err);
+            console.log("successful delete was made");
+            res.json({
+              statusL: true,
+              data: result
+            });
+    })
+    })
+
 
 
 app.listen(3000);
